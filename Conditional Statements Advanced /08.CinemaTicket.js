fce75f3079a28ccbeef3a4244dabbd48328d7084
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-08 13:57:11 +0300

    Create 08.CinemaTicket.js

---
+++ Conditional Statements Advanced /08.CinemaTicket.js
@@ -0,0 +1,16 @@
+function cinema(input) {
+    let day = input[0];
+    let price = 0;
+    switch (day) {
+        case "Monday":
+        case "Tuesday": console.log(price = 12); break;
+        case "Wednesday":
+        case "Thursday": console.log(price = 14); break;
+        case "Friday": console.log(price = 12); break;
+        case "Saturday":
+        case "Sunday": console.log(price = 16); break;
+        default: break;
+    }
+
+
+}
