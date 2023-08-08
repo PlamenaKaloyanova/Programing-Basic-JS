f255305fe5ffb814f78d3cb322c859c77173d40f
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-08 14:26:34 +0300

    Create 02.MultiplicationTable.js

---
+++ ForLoop/02.MultiplicationTable.js
@@ -0,0 +1,10 @@
+function demo(InputDeviceInfo) {
+    let x = Number(InputDeviceInfo[0]);
+
+    for (let i= 1; i<= 10; i++) {
+       
+        console.log(`${i} * ${x} = ${(i * x)}`)
+
+    }
+
+}
