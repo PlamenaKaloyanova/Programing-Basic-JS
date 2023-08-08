39bfaae4ae46b73c2dd516249899786016607e97
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-08 13:24:18 +0300

    Create 07ProjectsCreation.js

---
+++ First Steps In Coding/07ProjectsCreation.js
@@ -0,0 +1,7 @@
+function projectMaking(input) {
+    let arhitectName = input[0];
+    let projectNumber = Number(input[1]);
+    let oneProject = 3;
+    let requiredHours = projectNumber * oneProject;
+    console.log('The architect' + ' ' + arhitectName + ' ' + 'will need' + ' ' + requiredHours + ' ' + 'hours' + ' ' + 'to complete' + ' ' + projectNumber + ' ' + 'project/s.')
+}
