68037b91b0e2b5a38446263485ba13f57b98354d
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-08 14:31:21 +0300

    Create 01.ReadText.js

---
+++ While Loop/01.ReadText.js
@@ -0,0 +1,13 @@
+   function readText(input){
+    let index = 0;
+    let command = input[index];
+    index++;
+
+    while(command != "Stop"){
+        console.log(command);
+        command=input[index];
+        index++;
+        
+        }
+    
+}
