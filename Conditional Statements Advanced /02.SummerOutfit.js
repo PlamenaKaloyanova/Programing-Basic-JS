f1cd0a7bbfbd29ade0fa681d413b1b22eb0c12a4
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-08 14:13:32 +0300

    Create 02.SummerOutfit.js

---
+++ Conditional Statements Advanced /02.SummerOutfit.js
@@ -0,0 +1,32 @@
+function outfit(input) {
+    let degree = Number(input[0]);
+    let time = input[1];
+
+    let outfit = "";
+    let shoes = "";
+
+    if (degree >= 10 && degree <= 18) {
+        switch (time) {
+            case "Morning": outfit = "Sweatshirt", shoes = "Sneakers"; break;
+            case "Afternoon": outfit = "Shirt", shoes = "Moccasins"; break;
+            case "Evening": outfit = "Shirt", shoes = "Moccasins"; break;
+            default: break;
+        }
+    } else if (degree > 18 && degree <= 24) {
+        switch (time) {
+            case "Morning": outfit = "Shirt", shoes = "Moccasins"; break;
+            case "Afternoon": outfit = "T-Shirt", shoes = "Sandals"; break;
+            case "Evening": outfit = "Shirt", shoes = "Moccasins"; break;
+            default: break;
+        }
+
+    } else {
+        switch (time) {
+            case "Morning": outfit = "T-Shirt", shoes = "Sandals"; break;
+            case "Afternoon": outfit = "Swim Suit", shoes = "Barefoot"; break;
+            case "Evening": outfit = "Shirt", shoes = "Moccasins"; break;
+            default: break;
+        }
+    }
+    console.log(`It's ${degree} degrees, get your ${outfit} and ${shoes}.`);
+}
